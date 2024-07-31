69642d96f7f570c45d1893d09cbfa1a4d42bd459
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-07-31 15:16:33 +0300

    Lisst all games

--- client/src/components/games/Games.jsx
+++ client/src/components/games/Games.jsx
@@ -1,48 +1,26 @@
 import { Link } from "react-router-dom";
-import rdr from "../../../public/images/Red_Dead_Redemption_2.png";
-import fifa from "../../../public/images/Fifa24.png";
+
+import { useState, useEffect } from "react";
+import * as gamesAPI from "../../api/gamesApi";
+
+import GamesListItems from "./games-list-items/GamesListItems";
 
 export default function Games() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+       gamesAPI.getAll()
+            .then(result => setGames(result));
+    }, []);
+
     return (
         <section id="catalog">
             <h1>All Games</h1>
 
             <div className="border"></div>
-
-            <div className="allGames">
-                <div className="gamesInfo">
-                    <img src={rdr} />
-                    <div className="gameDescription">
-                        <h2>Game Name: Red Dead Redemption 2</h2>
-                        <h6>Category: Action</h6>
-                    </div>
-                    <div className="gameInfoBtns">
-                        <div className="likeAndDisLike">
-                            <button className="likeBtn">Like</button>
-                            <button className="disLikeBtn">Dislike</button>
-                        </div>
-                        <Link to="/games/details" className="detailsBtn">Details</Link>
-                    </div>
-                </div>
-            </div>
-
-            <div className="allGames">
-                <div className="gamesInfo">
-                    <img src={fifa} />
-                    <div className="gameDescription">
-                        <h2>Game Name: Fifa 2024</h2>
-                        <h6>Category: Sport</h6>
-                    </div>
-                    <div className="gameInfoBtns">
-                        <div className="likeAndDisLike">
-                            <button className="likeBtn">Like</button>
-                            <button className="disLikeBtn">Dislike</button>
-                        </div>
-                        <Link to="/games/details" className="detailsBtn">Details</Link>
-                    </div>
-                </div>
-            </div>
-
+            
+            {games.map(game => <GamesListItems key={game._id} {...game} />)}
+        
            
           <h3 className="no-articles">No articles yet</h3>
         </section>
---
+++ client/src/components/games/games-list-items/GamesListItems.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function GamesListItems({
+    _id,
+    title,
+    category,
+    imageUrl,
+}) {
+    return (
+        <div className="allGames">
+                <div className="gamesInfo">
+                    <img src={imageUrl}/>
+                    <div className="gameDescription">
+                        <h2>{title}</h2>
+                        <h6>{category}</h6>
+                    </div>
+                    <div className="gameInfoBtns">
+                        <div className="likeAndDisLike">
+                            <button className="likeBtn">Like</button>
+                            <button className="disLikeBtn">Dislike</button>
+                        </div>
+                        <Link to="/games/details" className="detailsBtn">Details</Link>
+                    </div>
+                </div>
+        </div>
+    );
+}
