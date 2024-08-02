a603d21a5b60dc71ae4e52026f508653549fef07
StelianY11 <134134591+StelianY11@users.noreply.github.com>
2024-08-02 16:59:21 +0300

    Add auth state and context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import { useState } from "react"
 
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
@@ -12,28 +13,43 @@ import Games from "./components/games/Games"
 import CreateGames from "./components/create-games/CreateGames"
 import GamesDetils from "./components/games-details/GamesDetails"
 import Edit from "./components/edit/Edit"
+import { AuthContext } from "./contexts/authContext"
 
 function App() {
+  const [authState, setAuthState] = useState({});
+
+  const changeState = (state) => {
+    setAuthState(state);
+  };
+
+  const contextData = {
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeState,
+  }
 
   return (
-    <div id="site">
-      <Header />
+    <AuthContext.Provider value={contextData}>
+      <div id="site">
+        <Header />
 
-      <main id="site-main">
+        <main id="site-main">
           <Routes>
-              <Route path="/" element={<Home />}/>
-              <Route path="/login" element={<Login />}/>
-              <Route path="/register" element={<Register />}/>
-              <Route path="/logout" element={<Logout />}/>
-              <Route path="/games" element={<Games />}/>
-              <Route path="/games/:gameId/details" element={<GamesDetils />}/>
-              <Route path="/games/details/edit" element={<Edit />}/>
-              <Route path="/games/create" element={<CreateGames />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/games/:gameId/details" element={<GamesDetils />} />
+            <Route path="/games/details/edit" element={<Edit />} />
+            <Route path="/games/create" element={<CreateGames />} />
           </Routes>
-      </main>
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   )
 }
 
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { login } from "../api/authApi"
+import { AuthContext } from "../contexts/authContext";
 
 export const useLogin = () => {
+    const {changeState} = useContext(AuthContext);
+
     const loginHandler = async (email, password) => {
             const result = await login(email, password);
+            changeState(result);
     }
 
     return loginHandler;
